554c2967e6ce9afffeff59391839a9d62f62189c
RaulJimenezOrtega <hhkaos@gmail.com>
2016-06-16 08:54:22 +0200

    Adding broken sample to AMQP server

---
+++ send-msg-to-amqp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import pika
+import json
+from config import *
+
+hostSend = 'amqp.emtmadrid.es'
+portSend = 5672
+credentSend = pika.PlainCredentials(idClient, passKey)
+
+connection = pika.BlockingConnection(
+  pika.ConnectionParameters(
+    hostSend, portSend, '/', credentSend
+  )
+)
+
+channel = connection.channel()
+channel.tx_select()
+
+message = '{ "Test Message" }'
+
+try:
+
+  channel.basic_publish(
+    exchange = '',
+    routing_key = 'messages',
+    body = message,
+    properties = pika.BasicProperties(
+      delivery_mode = 2, 
+      user_id = idClient
+    )
+  )
+
+  channel.tx_commit()
+
+except Exception as e:
+
+  print "The message can't be published"
+  print e
+
+print "Sent"
+connection.close()
