070557d537ff5f219b207b0dae60d87e321a3f99
RaulJimenezOrtega <hhkaos@gmail.com>
2016-06-16 12:22:46 +0200

    Adding to more samples

---
+++ connect-reative-box.py
@@ -0,0 +1,68 @@
+import time
+from MeteorClient import MeteorClient
+
+"""
+This code is a example for make a connection from a Reactive Box Mobility Madrid
+The Reactive Box is a Meteor Server which includes many layers of data.
+Current example shows the way of make subscription at PM 2.5  Polutans
+
+For use this program must be include:
+- MeteorClient https://github.com/hharnisc/python-meteor
+- EventEmitter https://github.com/jfhbrook/pyee
+- Websockets (ws4py on https://github.com/Lawouach/WebSocket-for-Python)
+- ddpClient https://pypi.python.org/pypi/python-ddp/0.1.0
+"""   
+
+def subscribed(subscription):
+    print('* SUBSCRIBED {}'.format(subscription))
+
+def unsubscribed(subscription):
+    print('* UNSUBSCRIBED {}'.format(subscription))
+
+def added(collection, id, fields):
+    print('* ADDED {} {}'.format(collection, id))
+
+    if collection == "users":
+        print collection
+        #client.subscribe('SENPM25MAD.eventpos.all')
+        client.subscribe('SENPM25MAD.eventpos.custom', [{'idStation': '28079008'}])
+
+    else:
+        print collection
+        print fields
+
+def changed(collection, id, fields, cleared):
+    print ("changed:{}".format(id) + " fields: {}".format(fields))
+
+def connected():
+    print('* CONNECTED')
+    client.login("test", "test")
+
+def subscription_callback(error):
+    if error:
+        print(error)
+
+try:
+    client = MeteorClient('ws://rbmobility.emtmadrid.es:3333/websocket', auto_reconnect = True, auto_reconnect_timeout = 5, debug = False)
+    client.on('subscribed', subscribed)
+    client.on('unsubscribed', unsubscribed)
+    client.on('added', added)
+    client.on('connected', connected)
+    client.on('changed',changed)   
+
+    client.connect()
+
+    # ctrl + c to kill the script
+
+    while True:
+        try:
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            break
+  
+    client.subscribe('SENPM25MAD.eventpos.custom',[{'idStation': '28079008'}])
+    #client.unsubscribe("ROUTESMAD.usrtrack.custom")
+
+except Exception as err :
+    print err.message
---
+++ myBus.py
@@ -0,0 +1,46 @@
+import urllib
+import urllib2
+import json 
+from config import *
+
+stopFindBusTest = 72
+
+urlGetFindBusTest = "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetArriveStop.php"
+
+params = {
+    'idClient' : idClient,
+    'passKey' : passKey,
+    'idStop' : stopFindBusTest
+}
+data = urllib.urlencode(params)
+
+headers = { 
+    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+    'Content-type': 'application/x-www-form-urlencoded' 
+}
+
+req = urllib2.Request(urlGetFindBusTest, data, headers)
+response = urllib2.urlopen(req)
+results = response.read()
+resultJson = json.loads(results)
+
+if resultJson.has_key('arrives'):
+    resultJsonbus = resultJson['arrives']
+    for results in resultJsonbus:
+        #now, we are going to use this bus number for getting data from myBus system
+        myBusTest = results['busId']
+        print "accesing to bus Id..." + myBusTest
+        urlbus = "https://mybus.emtmadrid.es:8073/rests"
+        params = "?srv=DatosCoche&Paradas=99&bus=" + myBusTest
+        aut_h = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        aut_h.add_password(None, urlbus,  idClient, passKey) 
+        handler = urllib2.HTTPBasicAuthHandler(aut_h)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+
+        response = urllib2.urlopen(urlbus + params)
+        results = response.read()
+        print results
+        #valueDict = xmltodict.parse(results)
+
+print "end"
