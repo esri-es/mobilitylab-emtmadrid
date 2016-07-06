3d0abc14d8181bf5a8e88045ec7cc940d16d4ad3
RaulJimenezOrtega <hhkaos@gmail.com>
2016-07-06 12:14:04 +0200

    Fixing query as Andres proposed at #4

--- bigdata.py
+++ bigdata.py
@@ -1,17 +1,16 @@
 import httplib
 import requests
 
-url="http://rbdata.emtmadrid.es:8443/DataProvider/api/dmz/getCollection/test/@EMT123/Layers/SENPM25MAD.eventpos/"
-query= { "idStation": "28079038", "$and": [ { "value": { "$gt": "00025" } } ] }
+url= "https://rbdata.emtmadrid.es:8443/DataProvider/api/dmz/getCollection/mobilitylabs.usertest/usertest/Layers/BUSTEST.eventpos/"
+query= "{ \"keyid\": \"9224\" }"
 
 headers = {
   'content-type': "application/json",
   'cache-control': "no-cache"
 }
-headers = {
-  'content-type': "application/json",
-  'cache-control': "no-cache"
-}
-response = requests.request("POST", url, data=query, headers=headers)
+
+response = requests.request("POST", url, data=query, headers=headers, verify=False)
 
 print(response.text)
+
+ 
