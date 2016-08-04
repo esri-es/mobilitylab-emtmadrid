d159ee9a1c26bfb125a35fb0baada79d3fd20f8f
RaulJimenezOrtega <hhkaos@gmail.com>
2016-08-04 11:05:04 +0200

    Removing old files

--- config/public_config.py
+++
@@ -1,7 +0,0 @@
-# Credentials for myBus.py
-idClient = 'EMT.SERVICIOS.OPENBUS'
-passKey = 'A2C983E5-5BA3-41F3-B47C-428F467041DC'
-
-# Credentials for AMQP
-idClientTest = 'mobilitylabs.usertest'
-passKeyTest = 'usertest'
